380a33c8cfca3325ebc3ea2e10070eb76924c812
Bo Marchman <bo.marchman@gmail.com>
2018-08-23 14:10:02 -0500

    Compute longest run times

--- analyze.py
+++ analyze.py
@@ -4,17 +4,30 @@ from master import load_pickle, hms
 
 if __name__ == "__main__":
     
-    ces = []
+    def concept_name(concept):
+        return ''.join(concept.node_labels.indices2labels(concept.mechanism))
+
     directory = 'largepyr_2.1_00000000000000000000'
+    ces = []
+    runtimes = {} 
+
+    print('CES:')
 
     for path in glob.glob(f'{directory}/*.pickle'):
         if 'network' not in path:
             concept = load_pickle(path)
             if concept.phi > 0:
                 ces.append(concept)
+            runtimes[concept_name(concept)] = concept.time
     
     for concept in ces:
         print("{:4}{:<12}{}".format(
-                ''.join(concept.node_labels.indices2labels(concept.mechanism)), 
+                concept_name(concept),
                 concept.phi,
                 hms(concept.time)))
+
+    print()
+    print('Longest computation times:')
+
+    for name, time in sorted(runtimes.items(), key=lambda x: x[1], reverse=True)[:20]:
+        print("{:4}{}".format(name, hms(time)))
