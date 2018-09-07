d0d0c84f9ddc37d649d7bd4e63e2344c2933d0e7
Bo Marchman <bo.marchman@gmail.com>
2018-09-07 14:15:49 -0500

    Remove largepyr master

--- master.py
+++ master.py
@@ -298,29 +298,3 @@ def start_master(experiment, mechanisms, port):
     print("Done")
     print("Total execution time: {}".format(hms(time() - start_time)))
 
-
-if __name__ == '__main__':
-
-    state = (0,) * 20  # TODO: pull from experiment
-    elements = list(range(20))
-    
-    def mechanisms_for_order(elements, n):
-        return pyphi.utils.combs(elements, n).tolist()
-
-    fourth = mechanisms_for_order(elements, 4)
-    random.shuffle(fourth)
-
-    mechanisms = itertools.chain(
-        mechanisms_for_order(elements, 1),
-        mechanisms_for_order(elements, 2),
-        mechanisms_for_order(elements, 3),
-        fourth
-    )
-    
-    # Already has a saved network file
-    experiment = Experiment('largepyr', '2.1', None, state)
-    port = 10006
-
-    with WorkerFactory(experiment) as factory:
-        start_master(experiment, mechanisms, port)
-
